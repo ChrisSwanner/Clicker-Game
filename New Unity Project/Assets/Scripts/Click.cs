05c1ae118e23245b55b3b62ed785baca52361e96
Chris Swanner <cmanswanner@gmail.com>
2018-03-01 10:18:54 -0800

    Initial commit

---
+++ New Unity Project/Assets/Scripts/Click.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text gpc;
+    public UnityEngine.UI.Text goldDisplay;
+    public float gold = 0;
+    public int goldperclick = 1;
+
+    private void Update()
+    {
+        goldDisplay.text = "Gold: " + gold;
+        gpc.text = goldperclick + " gold/click";
+    }
+
+    public void Clicked()
+    {
+        gold += goldperclick;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/goldpersec.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldpersec : MonoBehaviour {
+
+    public UnityEngine.UI.Text gpsText;
+    public Click click;
+    public ItemManager[] items;
+
+    private void Start()
+    {
+        StartCoroutine(AutoTick());
+    }
+
+    void Update()
+    {
+        gpsText.text = GetGPS() + " gold/sec";
+    }
+    public int GetGPS()
+    {
+        int tick = 0;
+        foreach (ItemManager item in items)
+        {
+            tick += item.count * item.tickValue;
+        }
+        return tick;
+    }
+
+    public void AutoGoldPerSec()
+    {
+        click.gold += GetGPS();
+    }
+
+    IEnumerator AutoTick()
+    {
+        while(true)
+        {
+            AutoGoldPerSec();
+            yield return new WaitForSeconds(1);
+        }
+    }
+		
+	}
